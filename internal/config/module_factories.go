59a5387685574a368707076637ff9d943a1daff9
Harsh Sharma <harsh@codelif.in>
2025-04-09 12:48:59 +0530

    modules(sep): changed to a space-sep-space conjuction

--- internal/config/module_factories.go
+++ internal/config/module_factories.go
@@ -2,12 +2,11 @@ package config
 
 import (
 	"github.com/codelif/pawbar/internal/modules"
+	"github.com/codelif/pawbar/internal/modules/backlight"
 	"github.com/codelif/pawbar/internal/modules/battery"
 	"github.com/codelif/pawbar/internal/modules/clock"
 	"github.com/codelif/pawbar/internal/modules/hyprtitle"
 	"github.com/codelif/pawbar/internal/modules/hyprws"
-	"github.com/codelif/pawbar/internal/modules/backlight"
-
 )
 
 var moduleFactories = map[string]func() modules.Module{
@@ -20,6 +19,6 @@ var moduleFactories = map[string]func() modules.Module{
 		return modules.NewStaticModule("space", []modules.EventCell{{C: ' ', Style: modules.DEFAULT}}, nil)
 	},
 	"sep": func() modules.Module {
-		return modules.NewStaticModule("sep", []modules.EventCell{{C: '│', Style: modules.DEFAULT}}, nil)
+		return modules.NewStaticModule("sep", []modules.EventCell{{C: ' ', Style: modules.DEFAULT}, {C: '│', Style: modules.DEFAULT}, {C: ' ', Style: modules.DEFAULT}}, nil)
 	},
 }
