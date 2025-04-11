5d211488f02a6877ee8616d482603df0802a06c2
Karvy Singh <karvysingh14@gmail.com>
2025-04-12 00:14:38 +0530

    feat: disk format change on click added

--- internal/modules/disk/disk.go
+++ internal/modules/disk/disk.go
@@ -1,94 +1,112 @@
 package disk
 
-import(
-	"time"
+import (
 	"fmt"
-	"os"
-	
+	"time"
+
 	"github.com/codelif/pawbar/internal/modules"
-  "github.com/shirou/gopsutil/v3/disk"
 	"github.com/codelif/pawbar/internal/utils"
 	"github.com/gdamore/tcell/v2"
-
+	"github.com/shirou/gopsutil/v3/disk"
 )
 
-func New() modules.Module {
-	return &DiskModule{}
+type DiskModule struct {
+	receive     chan bool
+	send        chan modules.Event
+	format      int
+	alterformat int
+	lastPref    int
+	visible     string
 }
 
-type DiskModule struct {
-	receive chan bool
-	send    chan modules.Event
-	format  int `default : "1" `
+func New() modules.Module {
+	d := &DiskModule{
+		format:      1,
+		alterformat: 3,
+		lastPref:    1,
+	}
+	d.Update(1)
+	return d
 }
 
 func (d *DiskModule) Dependencies() []string {
 	return nil
 }
 
-func (d *DiskModule) Update(format int) (value string ){
-	home := os.Getenv("HOME")
-	disk_, err := disk.Usage(home)
-	if err !=nil {
-		return ""
+func (d *DiskModule) formatSpecifier(value int) {
+	d.Update(value)
+	d.lastPref = value
+}
+
+func (d *DiskModule) Update(format int) {
+	du, err := disk.Usage("/")
+	if err != nil {
+		d.visible = ""
+		return
 	}
-	if format==2{
-		value2:= float64(disk_.Used)/1073741824.00
-		rstring := fmt.Sprintf(" %.2fGB", value2)
-		return rstring
+	if format == 1 {
+		d.visible = fmt.Sprintf(" %d%%", int(du.UsedPercent))
+	} else if format == 2 {
+		d.visible = fmt.Sprintf(" %.2fGB", float64(du.Used)/1073741824.0)
+	} else if format == 3 {
+		d.visible = fmt.Sprintf(" %d%%", 100-int(du.UsedPercent))
+	} else if format == 4 {
+		d.visible = fmt.Sprintf(" %.2fGB", float64(du.Free)/1073741824.0)
 	}
-	value1:= int(disk_.UsedPercent)
-	rstring := fmt.Sprintf(" %d%%", value1)
-	return rstring
 }
 
 func (d *DiskModule) Run() (<-chan bool, chan<- modules.Event, error) {
 	d.receive = make(chan bool)
 	d.send = make(chan modules.Event)
-
 	go func() {
 		t := time.NewTicker(7 * time.Second)
 		defer t.Stop()
 		for {
 			select {
 			case <-t.C:
-					d.receive <- true
-		
-			case e:=  <-d.send:
-			switch ev := e.TcellEvent.(type) {
-			case *tcell.EventMouse:
-				if ev.Buttons() != 0 {
-					x, y := ev.Position()
-					utils.Logger.Printf("Clock: Got click event: %d, %d, Mod: %d, Button: %d\n", x, y, ev.Modifiers(), ev.Buttons())
-					if d.format==1 {
-						d.format=2
-					}else{
-						d.format=1
+				d.receive <- true
+			case e := <-d.send:
+				switch ev := e.TcellEvent.(type) {
+				case *tcell.EventMouse:
+					if ev.Buttons() != 0 {
+						x, y := ev.Position()
+						btn := ev.Buttons()
+						utils.Logger.Printf("Disk: Got click event: %d, %d, Mod: %d, Button: %d\n", x, y, ev.Modifiers(), btn)
+						if btn == 1 {
+							if d.format == 1 {
+								d.format = 2
+								d.formatSpecifier(2)
+							} else {
+								d.format = 1
+								d.formatSpecifier(1)
+							}
+						} else if btn == 2 {
+							if d.alterformat == 3 {
+								d.alterformat = 4
+								d.formatSpecifier(4)
+							} else {
+								d.alterformat = 3
+								d.formatSpecifier(3)
+							}
+						}
 					}
 				}
 			}
-			}
-			}
-		
+		}
 	}()
-
 	return d.receive, d.send, nil
 }
 
 func (d *DiskModule) Render() []modules.EventCell {
+	d.Update(d.lastPref)
 	icon := ''
-	rstring:= d.Update(d.format)
-	r_ := make([]modules.EventCell, len(rstring)+1)
-	i := 0
-	r_[i] = modules.EventCell{C: icon, Style: modules.DEFAULT, Metadata: "", Mod: d}
-	i++
-	for _, ch := range rstring {
-		r_[i] = modules.EventCell{C: ch, Style: modules.DEFAULT, Metadata: "", Mod: d}
-		i++
+	r := make([]modules.EventCell, len(d.visible)+1)
+	r[0] = modules.EventCell{C: icon, Style: modules.DEFAULT, Metadata: "", Mod: d}
+	for i, ch := range d.visible {
+		r[i+1] = modules.EventCell{C: ch, Style: modules.DEFAULT, Metadata: "", Mod: d}
 	}
-	return r_
+	return r
 }
-
 
 func (d *DiskModule) Channels() (<-chan bool, chan<- modules.Event) {
 	return d.receive, d.send
@@ -97,3 +115,4 @@ func (d *DiskModule) Channels() (<-chan bool, chan<- modules.Event) {
 func (d *DiskModule) Name() string {
 	return "disk"
 }
+
