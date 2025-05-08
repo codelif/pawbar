958c6dbe9e0a82aeac2a609402fdd2d545db4984
Harsh Sharma <harsh@codelif.in>
2025-05-08 13:25:34 +0530

    install.sh: uncomment config making

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 sudo cp bar pawbar /usr/local/bin/
 mkdir -p "$HOME/.config/pawbar/"
 cp kitty.conf "$HOME/.config/pawbar/"
-# echo -e  "right:\n  - battery\n  - sep\n  - clock" > "$HOME/.config/pawbar/pawbar.yaml"
+echo -e  "right:\n  - battery\n  - sep\n  - clock" > "$HOME/.config/pawbar/pawbar.yaml"
