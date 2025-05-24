b46689d26f68cf53f2209389a3602a1542109fc3
Karvy Singh <karvysingh14@gmail.com>
2025-05-24 23:36:20 +0530

    modules(Locale): config added (known as carrying forward my mistakes)

--- internal/modules/locale/config.go
+++ internal/modules/locale/config.go
@@ -1,35 +1,38 @@
-package custom
+package locale
 
 import (
+	"time"
+
 	"github.com/codelif/pawbar/internal/config"
 	"github.com/codelif/pawbar/internal/modules"
 )
 
 func init() {
-	config.RegisterModule("custom", defaultOptions, func(o Options) (modules.Module, error) { return &CustomModule{opts: o}, nil })
+	config.RegisterModule("locale", defaultOptions, func(o Options) (modules.Module, error) { return &LocaleModule{opts: o}, nil })
 }
 
 type Options struct {
 	Fg      config.Color                      `yaml:"fg"`
 	Bg      config.Color                      `yaml:"bg"`
 	Cursor  config.Cursor                     `yaml:"cursor"`
-	Tick    config.Duration                   `yaml:"tick"`
 	Format  config.Format                     `yaml:"format"`
+	Tick    config.Duration                   `yaml:"tick"`
 	OnClick config.MouseActions[MouseOptions] `yaml:"onmouse"`
 }
 
 type MouseOptions struct {
 	Fg     *config.Color    `yaml:"fg"`
 	Bg     *config.Color    `yaml:"bg"`
-	Tick   *config.Duration `yaml:"tick"`
 	Cursor *config.Cursor   `yaml:"cursor"`
+	Tick   *config.Duration `yaml:"tick"`
 	Format *config.Format   `yaml:"format"`
 }
 
 func defaultOptions() Options {
-	f, _ := config.NewTemplate("")
+	f, _ := config.NewTemplate("{{.Locale}}")
 	return Options{
 		Format:  config.Format{Template: f},
+		Tick:    config.Duration(7 * time.Second),
 		OnClick: config.MouseActions[MouseOptions]{},
 	}
 }
