130310d0f1a57b1c311a513258f99890f1a7d5bf
Harsh Sharma <harsh@codelif.in>
2025-11-24 14:38:47 +0530

    update LICENSE information

--- internal/modules/mpris/mpris.go
+++ internal/modules/mpris/mpris.go
@@ -1,3 +1,9 @@
+// Copyright (c) 2025 Nekorg All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// SPDX-License-Identifier: bsd
+
 package mpris
 
 import (
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
+# Copyright (c) 2025 Nekorg All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+#
+# SPDX-License-Identifier: bsd
+
 
 sudo rm -f /usr/local/bin/pawbar
 
